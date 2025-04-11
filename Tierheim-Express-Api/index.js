d7c0c2f0f98af1e82a6d3b693aec7a9460758807
KAYT-Techstarter <marvin.young@tn.techstarter.de>
2025-04-11 12:17:27 +0200

    .

--- Tierheim-Express-Api/index.js
+++ Tierheim-Express-Api/index.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const app = express();
-const fs  = require("fs"); // File System Modul, damit können wir Dateien lesen und schreiben
+const fs = require("fs"); // File System Modul, damit können wir Dateien lesen und schreiben
 app.use(express.json()); // Unsere Middleware, die uns ermöglicht den Body aus dem Request auszulesen
 
 // Hilfsfunktion
-function readFile(){
+function readFile() {
     const data = fs.readFileSync("tiere.json", "utf-8");
-    return JSON.parse(data);}
+    return JSON.parse(data);
+}
 
-function writeFile(data){
-    fs.writeFileSync("tiere.json", JSON.stringify(data,null,2)); // JSON.stringify wandelt ein Javascript Objekt in eine JSON Format um
+function writeFile(data) {
+    fs.writeFileSync("tiere.json", JSON.stringify(data, null, 2)); // JSON.stringify wandelt ein Javascript Objekt in eine JSON Format um
 }
 
 app.get("/tiere", (req, res) => {
@@ -17,11 +18,11 @@ app.get("/tiere", (req, res) => {
     res.json(tiere);
 });
 
-app.post("/tiere" , (req,res) => {
+app.post("/tiere", (req, res) => {
     const tiere = readFile();
-    const {name, art} = req.body
-    
-    if(name && art){
+    const { name, art } = req.body
+
+    if (name && art) {
         const newTier = {
             id: tiere.length + 1, // besser (komplexere Logik) -> tiere.length > 0 ? Math.max(...tiere.map(a => a.id)) + 1 : 1;
             name: name,
@@ -35,5 +36,36 @@ app.post("/tiere" , (req,res) => {
         res.send("Daten unvollständig")
     }
 })
+
+app.put("/tiere/:id", (req, res) => {
+    const tiere = readFile();
+    const id = req.params.id;
+    const { name, art } = req.body;
+    const index = tiere.findIndex(tier => tier.id == id);
+    if (index !== -1) {
+        tiere[index].name = name;
+        tiere[index].art = art;
+        writeFile(tiere);
+        res.json(tiere[index]);
+    }
+    else {
+        res.send("Tier nicht gefunden");
+    }
+})
+
+app.delete("/tiere/:id", (req, res) => {
+    const tiere = readFile();
+    const id = req.params.id;
+    const index = tiere.findIndex(tier => tier.id == id);
+    if (index !== -1) {
+        tiere.splice(index, 1);
+        writeFile(tiere);
+        res.json(tiere);
+    }
+    else {
+        res.send("Tier nicht gefunden");
+    }
+})
+
 
 app.listen(5005);
